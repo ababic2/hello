584fd40307d197a3a066ee9ed65280a6a93deb54
ababic2 <ababic2@pop-os.localdomain>
2019-10-06 22:16:57 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/hello/Main.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Tarik");
+        Student student = new Student("Tarik", "Sijercic");
+        System.out.println(student);
+    }
+}
